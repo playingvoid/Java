381e471f87c82761cf4753e8df06fd9012307cb7
playingvoid <playingvoid@gmail.com>
2015-07-11 18:55:08 -0700

    Paring paranthesis complete

--- ParingParanthesis/ParingParanthesis.java
+++ ParingParanthesis/ParingParanthesis.java
@@ -4,59 +4,63 @@
  //and differences between s and s1 are minimum. 
  //Eg - "(ab(xy)u)2)" -> "(ab(xy)u)2" ")))(((" -> "" 
  
- int getRightParenCout(String input)
- {
-	int count == 0;
-	for(char c : input)
-		if(c == ')') count++;
-	return count;
- }
- String balancedParenthesis(String input)
- {
-	int rightMatches = getRightParenCount(input);
-	int rightRemaining = rightMatches;
-	int leftMatches = 0;
-	StringBuilder output = new StringBuilder();
-	for(int i=0; i< input.lenght; i++)
+public class ParingParanthesis 
+{
+	static int getRightParenCout(String input) 
 	{
-		char curr = input.charAt(i);
-		if(curr == '(')
+		int count = 0;
+		for (int i = 0; i < input.length(); i++) 
 		{
-			if(rightRemaining > 0 && rightMatches > 0)
-			{
-				leftMatches++;
-				rightMatches--;
-				output.append(curr);
-			}
+			if (input.charAt(i) == ')')
+				count++;
 		}
-		else if(curr == ')')
-		{
-			if(leftMatches > 0)
-			{
-				leftMatches--;
-				output.append(curr);
-			}
-			rightRemaining--;
-		}
-		else
-		{
-			output.append(curr);
-		}
+		return count;
 	}
-	return output.toString();
- }
- 
- Input:
- ()()()
- rr = 3, rm=3, lm =0
- -----
- rr=3, rm=2, lm=1, output = (
- -----
- rr=2, rm=2, lm=0, output = ()
- -----
- rr=2, rm=1, lm=1, output = ()(
- -----
- rr=1, rm=1, lm=0, output = ()()
- -----
- 
+
+	static String balancedParenthesis(String input) 
+	{
+		int rightMatches = getRightParenCout(input);
+		int rightRemaining = rightMatches;
+		int leftMatches = 0;
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) 
+		{
+			char curr = input.charAt(i);
+			if (curr == '(') 
+			{
+				if (rightRemaining > 0 && rightMatches > 0) 
+				{
+					leftMatches++;
+					rightMatches--;
+					output.append(curr);
+				}
+			} 
+			else if (curr == ')') 
+			{
+				if (leftMatches > 0) 
+				{
+					leftMatches--;
+					output.append(curr);
+				}
+				rightRemaining--;
+			} 
+			else 
+			{
+				output.append(curr);
+			}
+		}
+		return output.toString();
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("[" + ParingParanthesis.balancedParenthesis("(ab(xy)u)2)") + "]");
+		System.out.println("[" + ParingParanthesis.balancedParenthesis("()()()") + "]");
+		System.out.println("[" + ParingParanthesis.balancedParenthesis("(") + "]");
+		System.out.println("[" + ParingParanthesis.balancedParenthesis(")") + "]");
+		System.out.println("[" + ParingParanthesis.balancedParenthesis("()(") + "]");
+		System.out.println("[" + ParingParanthesis.balancedParenthesis(")))(((") + "]");
+	}
+}
+
  
