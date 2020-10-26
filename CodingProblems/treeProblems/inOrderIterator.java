41d206767a2f4564857850c40d2e2a6cbc30389a
playingvoid <playingvoid@gmail.com>
2020-10-25 21:02:32 -0700

    Add inorder iterator and level order serialization

--- CodingProblems/treeProblems/inOrderIterator.java
+++ CodingProblems/treeProblems/inOrderIterator.java
@@ -1,2 +1,64 @@
-package treeProblems;public class inOrderIterator {
+package treeProblems;
+
+import java.util.Stack;
+
+public class inOrderIterator {
+    private binaryTreeNode tracker;
+    private binaryTreeNode next;
+    private Stack<binaryTreeNode> stack;
+
+    public inOrderIterator(binaryTreeNode root)
+    {
+        tracker = root;
+        stack = new Stack<binaryTreeNode>();
+        setNext();
+    }
+
+    public binaryTreeNode next()
+    {
+        if(!hasNext())
+            throw new RuntimeException("No elements to iterate : Iterator nullified");
+        binaryTreeNode n = next;
+        setNext();
+        return n;
+    }
+
+    public boolean hasNext()
+    {
+        return null != next;
+    }
+
+    private void setNext()
+    {
+        next = null;
+        while(null != tracker || !stack.isEmpty())
+        {
+            if(null != tracker)
+            {
+                stack.push(tracker);
+                tracker = tracker.left;
+            }
+            else
+            {
+                next = stack.pop();
+                tracker = next.right;
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        binaryTreeNode root = binaryTreeNode.createBinaryTree(new Integer[] {1, 2, null, null, 3, null, null });
+        inOrderIterator ioItr = new inOrderIterator(root);
+        while(ioItr.hasNext())
+        {
+            binaryTreeNode next = ioItr.next();
+            System.out.print(next.value + "\t");
+        }
+
+        System.out.println();
+        System.out.println("SUCCESS");
+    }
+
 }
--- CodingProblems/treeProblems/levelOrderSerializeDeserialize.java
+++ CodingProblems/treeProblems/levelOrderSerializeDeserialize.java
@@ -1,2 +1,75 @@
-package treeProblems;public class levelOrderSerializeDeserialize {
+package treeProblems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class levelOrderSerializeDeserialize {
+
+    public static String serialize(binaryTreeNode root){
+        if(root == null)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        Queue<binaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            binaryTreeNode btNode = queue.poll();
+            if(btNode != null){
+                sb.append(btNode.value);
+                queue.add(btNode.left);
+                queue.add(btNode.right);
+            } else {
+                sb.append("#");
+            }
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+
+
+    public static binaryTreeNode deSerialize(String serialized){
+        if(serialized == null || serialized.length() == 0 || serialized == "#")
+            return null;
+
+        String[] splittedSerialized = serialized.split(" ");
+
+        binaryTreeNode root = new binaryTreeNode(Integer.parseInt(splittedSerialized[0]));
+
+        Queue<binaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int index = 1;
+        while(!queue.isEmpty()){
+            binaryTreeNode btNode = queue.poll();
+            if(btNode == null)
+                continue;
+            if(!splittedSerialized[index].equals("#"))
+                btNode.left = new binaryTreeNode(Integer.parseInt(splittedSerialized[index]));
+            else
+                btNode.left = null;
+            queue.add(btNode.left);
+            index++;
+            if(!splittedSerialized[index].equals("#"))
+                btNode.right = new binaryTreeNode(Integer.parseInt(splittedSerialized[index]));
+            else
+                btNode.right = null;
+            queue.add(btNode.right);
+            index++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args){
+        System.out.println("This is serialize desrialize");
+        binaryTreeNode btNode = binaryTreeNode.createBinaryTree(new Integer[]{
+                1, 2, 5,null,null,6, null,null,3,null,4,null,null
+        });
+        String serialized = serialize(btNode);
+        System.out.println(serialized);
+
+        binaryTreeNode btNode2 = deSerialize(serialized);
+        serialized = serialize(btNode2);
+        System.out.println(serialized);
+    }
 }
